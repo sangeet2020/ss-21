c4e72bb55525479587e4aeae2e9bc3c127895e02
Sangeet Sagar <15uec053@lnmiit.ac.in>
2021-04-30 12:30:49 +0200

    improvements done, ready to submit

--- snlp/hw1/exercise_2.py
+++ snlp/hw1/exercise_2.py
@@ -6,33 +6,37 @@ import matplotlib.pyplot as plt
 
 G = Generator(PCG64())
 
-def stick_breaking(n:int,alpha:float) -> np.ndarray:
-  """
-  Draws n samples from a stick-breaking process with beta distribution intensity alpha.
 
-  :param n: number of samples
-  :param alpha: intensity parameter of the beta distribution
-  :returns: stick lengths
-  """
-  betas = G.beta(a=alpha, b=1.0, size=n)
-  betas[1:] *= np.cumprod(1-betas[:-1])
-  weights = np.sort(betas)[::-1]
-  return weights
+def stick_breaking(n: int, alpha: float) -> np.ndarray:
+    """
+    Draws n samples from a stick-breaking process with beta distribution intensity alpha.
 
-def plot_stick_lengths(stick_lengths:np.array,
-		       alpha:float,
-		       B:int) -> None:
-  """
-  Plots -log2(sticks)
-  :param sticks: list of stick lenghts
-  """
-  fig, ax = plt.subplots()
-  # word_pos = range(0, len(stick_lengths))
-  stick_lengths_log = [-B*math.log2(x) for x in stick_lengths]
-  ax.loglog(stick_lengths_log)
-  # ax.loglog(word_pos, stick_lengths_log)
-  ax.set_xlabel("rank")
-  ax.set_ylabel("log(stick length)")
-  ax.grid()
-  plt.show()
-  pass
+    :param n: number of samples
+    :param alpha: intensity parameter of the beta distribution
+    :returns: stick lengths
+    """
+    betas = G.beta(a=alpha, b=1.0, size=n)
+    betas[1:] *= np.cumprod(1-betas[:-1])
+    weights = np.sort(betas)[::-1]
+    return weights
+
+
+def plot_stick_lengths(stick_lengths: np.array,
+                       alpha: float,
+                       B: int) -> None:
+    """
+    Plots -log2(sticks)
+    :param sticks: list of stick lenghts
+    """
+    _, ax = plt.subplots()
+    idx = range(1, len(stick_lengths)+1)
+
+    ideal_zipf = [1/x for x in idx]
+    ax.plot(idx, ideal_zipf, 'g', label='Ideal Zipfian')
+
+    # f = m/[r^B]
+    stick_lengths_log = [x**B for x in stick_lengths]
+    ax.loglog(idx, stick_lengths_log, "r*", base=2, label="stick length")
+    ax.set_xlabel("rank")
+    ax.set_ylabel("stick length")
+    ax.legend()
--- snlp/hw1/exercise_3.py
+++ snlp/hw1/exercise_3.py
@@ -1,35 +1,46 @@
+import matplotlib as mpl
 import nltk
 import operator
+import numpy as np
 import matplotlib.pyplot as plt
+import seaborn
+seaborn.set()
+mpl.rcParams['figure.dpi'] = 100
+
 
 def analysis(name, data):
-  """
-  Plot Zipfian distribution of data + true Zipfian distribution. Compute MSE.
+    """
+    Plot Zipfian distribution of data + true Zipfian distribution. Compute MSE.
 
-  :param name: title of the graph
-  :param data: list of data
-  """
-  # print("TODO", name)
-  
-  frq_dict = nltk.FreqDist(data)
-  
-  # Sorting list in descending order of frequency
-  frq_dict = dict(sorted(frq_dict.items(), key=operator.itemgetter(1), reverse=True))
-  
-  # Separate out word frequencies and word into lists.
-  frq_list = list(frq_dict.values())
-  word_list = list(frq_dict.keys())
-  word_pos = range(0, len(word_list))
-  # word_pos = range(1, len(word_list)+1) ## I think, this is correct, as log(0) is undefined.
-  
-  # import pdb; pdb.set_trace()
-  
-  fig, ax = plt.subplots()
-  ax.loglog(word_pos, frq_list, label=str(name))
-  ax.set_ylabel("log(frequency)")
-  ax.set_xlabel("log(rank)")
-  ax.set_title("Frequency Curve with log scaling: "+str(name))
-  ax.grid()
+    :param name: title of the graph
+    :param data: list of data
+    """
 
-  # Compute mean squared error
-  # predicted_freq = [1/x for x in ]
+    frq_dict = nltk.FreqDist(data)
+    frq_dict = dict(
+        sorted(frq_dict.items(), key=operator.itemgetter(1), reverse=True))
+
+    frq_list = list(frq_dict.values())
+    word_list = list(frq_dict.keys())
+    word_pos = range(1, len(word_list)+1)  # Since as log(0) is undefined.
+
+    # Ideal Zipfian
+    m = frq_list[0]
+    ideal_zipf = [m*1/x for x in word_pos]
+    # import pdb; pdb.set_trace()
+
+    # Compute mean squared error
+    mse_np = np.sum(np.square(np.array(ideal_zipf) -
+                              np.array(frq_list)))/len(ideal_zipf)
+    print('MSE for %s: %.10f' % (name, mse_np))
+
+    _, ax = plt.subplots()
+    plt.figure(num=None, figsize=(9.5, 6), dpi=300,
+               facecolor='r', edgecolor='k')
+
+    ax.plot(word_pos, ideal_zipf, 'g', label='Ideal Zipfian')
+    ax.loglog(word_pos, frq_list, 'r+', label=str(name))
+    ax.set_ylabel("frequency")
+    ax.set_xlabel("rank")
+    ax.set_title("Frequency Curve with log scaling: "+str(name))
+    ax.legend()
