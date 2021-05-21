c1de32fc100cf2f64b5b8f1e1b7c4d742e478d60
nik1806 <nikhil.paliwal14@gmail.com>
2021-05-21 23:35:17 +0200

    complete 2.3

--- snlp/Assignment4/exercise_2.py
+++ snlp/Assignment4/exercise_2.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 from typing import Dict, List, Tuple
+import matplotlib.pyplot as plt
 
 
 def sample_records(genome_loc: Path, genome_red_loc: Path, num_records: int):
@@ -31,6 +32,23 @@ def get_k_mers(genome_red_loc: Path, k: int) -> List[str]:
     :param k: length of of the n-mer
     :return: a list of n-mers
     """
+    # Read
+    sequence = SeqIO.parse(genome_red_loc, 'fasta')
+    sequence = list(sequence)
+    # sequence = list(sequence)[:3]
+    # Combine
+    text = ''
+    for s in sequence:
+        text += str(s.seq)
+    text = text.upper()
+    # make k-mers
+    kmers_list = []
+    for i in range(len(text)):
+        if i+k >= len(text):
+            break
+        kmers_list.append(text[i:i+k])
+
+    return kmers_list
 
 
 def get_k_mers_24(genome_red_loc: Path, k: int, tandem_repeats=False) -> List[str]:
@@ -53,6 +71,34 @@ def k_mer_statistics(genome_red_loc: Path, K: int, delta=1.e-10) -> Tuple:
     :param delta: threshold for probability mass loss, defaults to 1.e-10
     :return: lists of relative frequencies and conditional probabilities
     """
+    Kmer_rel_freq = {}
+    Kmer_cond_prob = {}
+
+    for i in range(1,K+1):
+        kmers_list = get_k_mers(genome_red_loc, i)
+        # print(Kmer_cond_prob)
+        # relative frequency
+        rel_freq = {}
+        for kmer in kmers_list:
+            rel_freq[kmer] = kmers_list.count(kmer)/len(kmers_list)
+
+        Kmer_rel_freq[i] = rel_freq # all rel freq for one k-mer
+
+        if i == 1: # condition prob is equal to relative frequency K=1
+            cond_prob = rel_freq
+            Kmer_cond_prob[i] = cond_prob
+        else:
+
+            # conditional probability
+            cond_prob = {}
+            for kmer in kmers_list:
+                cond_prob[kmer] = rel_freq[kmer]/Kmer_rel_freq[i-1][kmer[:-1]]
+
+            Kmer_cond_prob[i] = cond_prob # all cond prob for one k-mer
+
+    return Kmer_rel_freq, Kmer_cond_prob
+
+    
 
 
 def k_mer_statistics_24(genome_red_loc: Path, K: int, tandem_repeats=False, delta=1.e-10) -> Tuple:
@@ -76,15 +122,41 @@ def conditional_entropy(rel_freqs: Dict, cond_probs: Dict) -> float:
     :param cond_probs: (a dictionary of) conditional probabilities
     :return: the conditional entropy of the corpus
     """
+    con_ent = 0
+    for k in rel_freqs.keys():
+        con_ent += -rel_freqs[k]*np.log2(cond_probs[k])
+
+    return con_ent
 
 
-def plot_k_mers(rel_freqs: List[Dict], n=10, k=5):
+def plot_k_mers(rel_freqs, n=10, k=5):
     """ Plots n most frequent k-mers vs. their frequency.
 
     :param rel_freqs: the list of relative frequency dicts
     :param n: the number of most frequent k-mers to plot
     :param k: the k of k-mers
     """
+    fig, ax = plt.subplots(k)
+
+    for i in range(1, k+1):
+        # for each k
+        rel_freq = rel_freqs[i]
+
+        kmers_sorted = {key: val for key, val in sorted(rel_freq.items(), key=lambda item: item[1], reverse=True)}
+        
+        #  Pick-out the most frequent top n kmers
+        kmers = list(kmers_sorted.keys())[:n]
+        freq = list(kmers_sorted.values())[:n]
+
+        ax[i-1].loglog(kmers, freq)
+        ax[i-1].set_xlabel("kmers")
+        ax[i-1].set_ylabel("frequency")
+        ax[i-1].set_title("K="+str(i))
+        
+
+    # plt.xticks(rotation=60, ha='right')   
+    plt.show()
+        
 
 
 def plot_conditional_entropies(H_ks:List[float]):
@@ -92,3 +164,7 @@ def plot_conditional_entropies(H_ks:List[float]):
 
     :param H_ks: the conditional entropy scores
     """
+    plt.plot(range(1,len(H_ks)+1), H_ks)
+    plt.xlabel("k-mer length")
+    plt.ylabel("Conditional entropy")
+    plt.show()
