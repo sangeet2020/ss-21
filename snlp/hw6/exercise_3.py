39e97f413a61f948370d6a7f55ed03db171925ee
Sangeet Sagar <15uec053@lnmiit.ac.in>
2021-06-04 19:38:27 +0200

    almost finished

--- snlp/hw6/exercise_3.py
+++ snlp/hw6/exercise_3.py
@@ -1,5 +1,8 @@
 from typing import List
-
+from random import randrange
+from random import sample
+import pdb
+import matplotlib.pyplot as plt
 
 def train_test_split_data(text: List[str], test_size=0.2):
     """ Splits the input corpus in a train and a test set
@@ -7,10 +10,33 @@ def train_test_split_data(text: List[str], test_size=0.2):
     :param test_size: size of the test set, in fractions of the original corpus
     :return: train and test set
     """
+    k = int(len(text) * (1 - test_size))
+    return text[:k], text[k:]
 
-def k_validation_folds(text: List[str], k_folds=10):
+def k_validation_folds(text: List[str], k_folds=5):
     """ Splits a corpus into k_folds cross-validation folds
     :param text: input corpus
     :param k_folds: number of cross-validation folds
     :return: the cross-validation folds
+    """     
+    chunk = len(text) / float(k_folds)
+    results = []
+    i = 0.0
+
+    while i < len(text):
+        results.append(text[int(i):int(i + chunk)])
+        i += chunk
+
+    return results
+
+def plot_pp_vs_alpha(pps: List[float], alphas: List[float]):
+    """ Plots n-gram perplexity vs alpha
+    :param pps: list of perplexity scores
+    :param alphas: list of alphas
     """
+    plt.plot(alphas, pps)
+    plt.xlabel('Alpha values')
+    plt.ylabel('Perplexity')
+    plt.title("Alpha vs Perplexity")
+
+    plt.show()
---
+++ snlp/hw6/lm.py
@@ -0,0 +1,157 @@
+import pdb
+import numpy as np
+import collections
+from copy import deepcopy
+from typing import List
+from collections import Counter
+
+from numpy.lib.financial import ppmt
+
+
+class LanguageModel:
+    
+    def __init__(self, train_tokens: List[str], test_tokens: List[str], N: int, alpha: float, epsilon=1.e-10):
+        """ 
+        :param train_tokens: list of tokens from the train section of your corpus
+        :param test_tokens: list of tokens from the test section of your corpus
+        :param N: n of the highest-order n-gram model
+        :param alpha: pseudo count for lidstone smoothing
+        :param epsilon: threshold for probability mass loss, defaults to 1.e-10
+        """
+        self.train_tokens = train_tokens
+        self.test_tokens = test_tokens
+        self.N = N
+        self.alpha = alpha
+        
+
+    def perplexity(self):
+        """ returns the perplexity of the language model for n-grams with n=n """
+        self.train_ngrams = self.get_ngrams(self.train_tokens, self.N)
+        self.test_ngrams = self.get_ngrams(self.test_tokens, self.N)
+        
+        rel_freq = self.relative_freq(self.test_ngrams)
+        smoothed_probs = self.lidstone_smoothing(self.test_ngrams)
+        summ = 0
+        for test_ngram, rf in rel_freq.items():
+            summ += rf * np.log2(smoothed_probs[test_ngram])
+        pp = 2**(-summ)
+        return pp
+
+    
+    def get_ngrams(self, tokens, N):
+        if N == 2:
+            tokens.append(tokens[0])
+        elif N == 3:
+            tokens.extend([tokens[0], tokens[1]])
+        
+        ngrams = zip(*[tokens[i:] for i in range(N)])
+        ngrams_list =  [" ".join(ngram) for ngram in ngrams]
+        return ngrams_list
+
+    
+    def get_ngrams_counts(self):
+        self.train_ngram_counts = Counter(self.train_ngrams)
+        self.test_ngram_counts = Counter(self.test_ngrams)
+        
+        for k,v in self.test_ngram_counts.items():
+            if k not in self.train_ngram_counts:
+                self.train_ngram_counts[k] = 0
+                
+        return self.train_ngram_counts
+        
+    def history_counts(self, tokens):
+        # pdb.set_trace()        
+        N = self.N - 1
+        history = self.get_ngrams(tokens, N)
+        history_counts = Counter(history)
+        return history_counts
+    
+    def compute_V(self):
+        # N = 2
+        # train_ngrams = self.get_ngrams(self.train_tokens, N)
+        # test_ngrams = self.get_ngrams(self.test_tokens, N)
+        
+        # train_ngram_counts = Counter(train_ngrams)
+        # test_ngram_counts = Counter(test_ngrams)
+        
+        # for k,v in test_ngram_counts.items():
+        #     if k not in train_ngram_counts:
+        #         train_ngram_counts[k] = 0
+        # # pdb.set_trace()   
+        # return train_ngram_counts
+        N = 3
+        ngram_counts = []
+        for n in range(1, N+1):
+            ngrams = self.get_ngrams(self.train_tokens, n)
+            ngram_counts.append(Counter(ngrams))
+            
+        V = set(ngram_counts[0].keys())
+        
+        V = V.union([test_token for test_token in self.test_tokens])
+        return V
+        
+    def lidstone_smoothing(self, ngrams):
+        if self.N != 1:
+            history_counts = self.history_counts(self.train_tokens)
+        ngram_counts = self.get_ngrams_counts()
+        
+        V = len(self.compute_V())
+        # if self.N == 1:
+        # V = len(ngram_counts)
+        # else:
+        # v_prev = self.history_counts(self.test_tokens)
+        
+        p_w = collections.defaultdict(float)
+        for ngram in ngrams:
+            # pdb.set_trace()
+            history = ' '.join(ngram.split()[:self.N-1])
+            if len(history) == 0:
+                history_count = len(self.train_ngrams)
+            else:
+                history_count = history_counts[history]        
+                
+            # pdb.set_trace()
+            p_w[ngram] = ( ngram_counts[ngram] + self.alpha ) / ( history_count + self.alpha*V )
+        # pdb.set_trace()    
+        return p_w
+    
+    def relative_freq(self, ngrams):
+        ngram_counts = Counter(ngrams)
+        rel_freq = collections.defaultdict(float)
+        for token, counts in ngram_counts.items():
+            rel_freq[token] = counts / len(ngrams)
+        # pdb.set_trace()
+        return rel_freq
+            
+            
+
+# from importlib import reload
+# import os
+# import exercise_2
+# import exercise_1
+
+# exercise_1 = reload(exercise_1)
+# exercise_2 = reload(exercise_2)
+
+
+# corpora = {} # To save the respective corpora
+# # TODO: Add a loop over each file
+# for filename in os.listdir('data/'):
+#     with open(os.path.join('data/', filename)) as f:
+#         text = f.read()
+#         pp = exercise_1.preprocess(text) #TODO: preprocess text
+#         train, test = exercise_1.train_test_split_data(pp, test_size=0.3) #TODO: split data
+#         #TODO: Add respective splits to the corpora dict
+#         lang = filename.split('.')[1]
+#         corpora[lang] = (train, test)
+
+# N = 3
+# PPs = {}
+# for lang, (train, test) in corpora.items():
+#     pp_list = []
+#     for i in range(1,N+1):
+#         LM = LanguageModel(train, test, N=i, alpha=1)
+#         pp_list.append(LM.perplexity())
+#     PPs[lang] = pp_list
+# # pdb.set_trace()
+# exercise_2.plot_pp(PPs)
